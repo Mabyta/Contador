655b7cc0e25fda28c34afeca23cb113c43052002
Maria Luisa Gonzalez <maria.gonzalez@uabc.edu.mx>
2018-09-24 13:44:24 -0700

    Menus

--- app/src/main/java/com/lsc/a1140676/mabel/contador/MainActivity.java
+++ app/src/main/java/com/lsc/a1140676/mabel/contador/MainActivity.java
@@ -2,12 +2,17 @@ package com.lsc.a1140676.mabel.contador;
 
 import android.app.AlertDialog;
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,10 +29,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         texto=(TextView) findViewById(R.id.texto);
         edittext=(EditText) findViewById(R.id.buscar);
+        registerForContextMenu(texto);
         if(savedInstanceState!=null){
             contador=Integer.parseInt(savedInstanceState.getString("count"));
         }
         texto.setText("Contador= "+contador);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo contextInfo){
+        super.onCreateContextMenu(menu,v,contextInfo);
+        getMenuInflater().inflate(R.menu.menu_context,menu);
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.itemContextEdit:
+                contador=0;
+                texto.setText("Contador= "+contador);
+                Toast.makeText(getApplicationContext(),"Se limpio el contador",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.itemContextShow:
+                Toast.makeText(getApplicationContext(),"El contador esta en "+contador,Toast.LENGTH_SHORT).show();
+                return true;
+                default:
+                    return super.onContextItemSelected(item);
+        }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(getApplicationContext(), "Valor 1",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(getApplicationContext(),"Valor 2",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(getApplicationContext(),"Hola",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item4:
+                Toast.makeText(getApplicationContext(),"Salvate tu solo",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item5:
+                Toast.makeText(getApplicationContext(),"Valor 5",Toast.LENGTH_SHORT).show();
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
     }
 
     public void iniciarIntent(View view){
